70f3df831a57f42c34f941e74201adf61dff4c94
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-08-11 12:55:40 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins:  [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ]
+}
